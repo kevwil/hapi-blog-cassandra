351a444fab0afc4a735be6fc000c6d3c1702bc79
Kevin Williams <kevwil@gmail.com>
2015-01-07 08:38:57 -0700

    update the code for the latest versions of technologies used

--- index.js
+++ index.js
@@ -1,47 +1,19 @@
-var helenus = require('helenus'),
-    pool = new helenus.ConnectionPool({
-      hosts: ['localhost:9160'],
-      keyspace: 'mykeyspace',
-      timeout: 3000
-    });
+var util = require('util'),
+    assert = require('assert'),
+    Joi = require('joi'),
+    Boom = require('boom'),
+    Hapi = require('hapi'),
+    server = new Hapi.Server({connections: {routes: {cors: true}}}),
+    cassandra = require('cassandra-driver'),
+    client = new cassandra.Client({contactPoints: ['localhost'], keyspace: 'mykeyspace'});
 
-var util = require('util');
-
-pool.on('error', function(err){
-  console.error(err.name, err.message);
+server.connection({port:8080});
+server.views({
+    engines: { html: require('handlebars')},
+    path: 'templates'
 });
 
-var Hapi = require('hapi');
-var serverOpts = {
-  views: {
-    path: 'templates',
-    engines: {
-      html: 'handlebars'
-    }
-  },
-  cors: true
-};
-
-function row2object(row){
-  return {
-    id: row.get('id').value,
-    title: row.get('title').value,
-    tags: row.get('tags').value,
-    content: row.get('content').value,
-    date: row.get('date').value
-  };
-}
-
-function results2array(results){
-  var output = [];
-  results.forEach(function(row){
-    output.push(row2object(row));
-  });
-  return output;
-}
-
-var server = Hapi.createServer('localhost', 8080, serverOpts);
-server.pack.require('lout', { endpoint: '/docs' }, function (err) {
+server.register({register: require('lout'), options: { endpoint: '/docs' }}, function (err) {
   if (err) {
     console.error('Failed loading plugin: lout');
   }
@@ -50,60 +22,41 @@ server.pack.require('lout', { endpoint: '/docs' }, function (err) {
 server.route([{
   method: 'GET',
   path: '/status',
-  handler: function() {
+  handler: function(request,reply) {
     reply('ok');
   }
 }, {
   method: 'GET',
   path: '/favicon.ico',
-  handler: function() {
-    this.reply().code(404);
+  handler: function(request,reply) {
+    reply(Boom.notFound());
   }
 }, {
   method: 'GET',
   path: '/blog',
-  handler: function(request) {
-    pool.connect(function(err,keyspace){
-      if(err){
-        throw(err);
-      }else{
-        // ORDER BY date DESC
-        pool.cql("SELECT * FROM posts", function(err,results){
-          if(err){
-            throw(err);
-          }else{
-            request.reply.view('posts.html', {results: results2array(results)});
-          }
-        });
-      }
+  handler: function(request,reply) {
+    client.execute("SELECT * FROM posts", function(err,results){
+      assert.ifError(err);
+      reply.view('posts.html', {results: results});
     });
   }
 }, {
   method: 'GET',
   path: '/blog/{title}',
   config: {
-    handler: function(request) {
-      pool.connect(function(err,keyspace){
-        if(err){
-          throw(err);
-        }else{
-          pool.cql("SELECT * FROM posts WHERE title = ?", [request.params.title], function(err,result){
-            if(err){
-              throw(err);
-            }else{
-              if(result.length < 1){
-                request.reply(new Error('no results'));
-              }else{
-                request.reply.view('post.html', row2object(result[0]));
-              }
-            }
-          });
-        }
+    handler: function(request,reply) {
+      client.execute("SELECT * FROM posts WHERE title = ?", [request.params.title], function(err,result){
+          assert.ifError(err);
+          if(result.length < 1){
+            reply(new Error('no results'));
+          }else{
+            reply.view('post.html', result[0]);
+          }
       });
     },
     validate: {
-      path: {
-        title: Hapi.types.String().required()
+      params: {
+        title: Joi.string().required()
       }
     }
   }
@@ -111,28 +64,27 @@ server.route([{
   method: 'GET',
   path: '/{name}',
   config: {
-    handler: function(request) {
+    handler: function(request,reply) {
       var greet = 'nice' in request.query && request.query.nice === 'true' ? 'Hello ' : 'Piss off ';
-      request.reply.view('index.html', {greeting: greet + request.params.name});
+      reply.view('index.html', {greeting: greet + request.params.name});
     },
     validate: {
-      path: {
-        name: Hapi.types.String().required()
+      params: {
+        name: Joi.string().required()
       },
       query: {
-        nice: Hapi.types.Boolean().allow(null)
+        nice: Joi.boolean().allow(null)
       }
     }
   }
 }, {
   method: 'GET',
   path: '/',
-  handler: function(request) {
-    request.reply.view('index.html', {greeting: 'Hello Anonymous Coward!'});
+  handler: function(request,reply) {
+      reply.view('index.html', {greeting: 'Hello Anonymous Coward!'});
   }
 }]);
 
 server.start( function() {
   console.log('Hapi server started at: ' + server.info.uri);
 });
-
